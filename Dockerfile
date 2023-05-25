1cb97a22b05c020a788154734647327d0583d1e2
majjangjjang <majjangjjang@github.com>
2023-05-25 23:01:46 +0900

    github to ECR

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,12 @@
-# Dockerfile
-
-FROM node:14
+FROM node:14-alpine
 
 WORKDIR /app
 
 COPY package.json .
-RUN npm install
+RUN npm install --production
 
 COPY . .
 
 EXPOSE 3000
 
-CMD [ "node", "server.js" ]
-
+CMD [ "npm", "start" ]
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
