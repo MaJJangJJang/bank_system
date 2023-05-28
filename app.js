e8ee23aa78f9dfceb8279790cf25f447788fd61e
majjangjjang <majjangjjang@github.com>
2023-05-28 21:47:10 +0900

    CI/CD update test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World 23.05.28 21:23 !');
+  res.send('Hello, World 23.05.28 21:47!');
 });
 
 app.listen(3000, () => {
