89ee9290941b1870230e24804491cb23be692093
majjangjjang <majjangjjang@github.com>
2023-05-28 18:15:59 +0900

    New workflow test

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,10 @@
-FROM node:14-alpine
+FROM node:14
 
 WORKDIR /app
 
-COPY package.json package-lock.json ./
-RUN npm install 
+COPY package.json ./
+RUN npm install
 
 COPY . .
 
-EXPOSE 3000
-
-CMD [ "npm", "start" ]
+CMD ["node", "app.js"]
--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(3000, () => {
+  console.log('서버가 실행 중입니다.');
 });
