6db3d4bcae317df9638a04517af7839400d20171
majjangjjang <majjangjjang@github.com>
2023-05-28 21:26:49 +0900

    CI/CD update test

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, World 23.05.28 21:23 !');
 });
 
 app.listen(3000, () => {
-  console.log('서버가 실행 중입니다.');
+  console.log('서버가 3000 port에서 실행 중입니다.');
 });
